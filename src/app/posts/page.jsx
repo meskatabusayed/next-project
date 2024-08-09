740ff6e1aebb616bd452222f770586b719122aa2
meskatabusayed <meskatabusayed@gmail.com>
2024-08-09 15:06:20 +0530

    dynamic routing ok

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 const getPosts = async () => {
@@ -14,15 +15,18 @@ const page = async () => {
   return (
     <div>
       <h6 className='text-center text-3xl font-bold'>All Posts</h6>
+      <div className='grid grid-cols-3 gap-5'>
       {
         postData?.map((post) => (
-            <div key={post.it} className='border-2 p-6'>
-                <h6>{post.title}</h6>
-                <h6>{post.description}</h6>
-
+            <div key={post.id} className='border-2 p-6 border-red-600'>
+                <h1>{post.id}</h1>
+                <h6>title : {post.title}</h6>
+                <h6>body : {post.body}</h6>
+                <button className='bg-blue-700 p-2 text-white rounded-md font-bold'><Link href={`/posts/${post.id}`}>See Deatils</Link></button>
             </div>
         ))
       }
+      </div>
     </div>
   )
 }
