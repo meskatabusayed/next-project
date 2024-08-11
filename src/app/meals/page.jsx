f2f0dff941c75896df9d14d6e0226d47bc1fd513
meskatabusayed <meskatabusayed@gmail.com>
2024-08-11 15:31:59 +0530

    styling added

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,11 +1,11 @@
 import Meals from '@/components/Meals';
 import React from 'react'
-
+import styles from './styles.module.css';
 const page = () => {
   return (
     <div className='p-12'>
       <h1>Choose Your Meals</h1>
-      <p>Choose meals of your choice by serching...</p>
+      <p className={styles.tomato}>Choose meals of your choice by serching...</p>
       <Meals/>
     </div>
   )
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,47 +1,59 @@
-"use client"
-import React, { useEffect, useState }  from 'react'
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
 const Meals = () => {
-  const [search , setSearch] = useState("a");
-  const [meals , setMeals] = useState([]);
+  const [search, setSearch] = useState("a");
+  const [error, setError] = useState("");
+  const [meals, setMeals] = useState([]);
   
 
-  const loadData = async() => {
-    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`);
-    const data = await res.json();
-    setMeals(data.meals);
-  }
+  const loadData = async () => {
+    try {
+      const res = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`
+      );
+      const data = await res.json();
+      setMeals(data.meals);
+      setError("");
+    } catch (error) {
+      setError("No Data Found...");
+    }
+  };
 
   const handler = (e) => {
     console.log(e.target.value);
     setSearch(e.target.value);
+  };
 
+  // useEffect(() => {
+  //   loadData();
+  // }, [search]);
 
-  }
-
-  useEffect(() => {
-     loadData();
-  } , [search]);
-  
   return (
     <div>
-      <input 
-      onChange={handler}
-      type="text" placeholder='search meals...'/>
-      <button onClick={() => loadData()} className='bg-blue-700 p-2 text-white rounded-md font-bold'>Search</button>
-      <div className='mt-12 grid grid-cols-3 gap-6'>
-        {
+      <input onChange={handler} type="text" placeholder="search meals..." />
+      <button
+        onClick={() => loadData()}
+        className="bg-blue-700 p-2 text-white rounded-md font-bold"
+      >
+        Search
+      </button>
+      <div className="mt-12 grid grid-cols-3 gap-6">
+        { meals?.length > 0 && !error &&
           meals?.map((meal) => (
-            <div key={meal.idMeal} className='border-2 p-4'>
+            <div key={meal.idMeal} className="border-2 p-4">
+              <Image src={meal?.strMealThumb} alt={meal.strMeal} width={500} height={500}/>
               <h6>{meal.strMeal}</h6>
               <p>{meal.strInstructions}</p>
             </div>
-          ))
-        }
-
+          ))}
+          {
+            error && <h6>No data Found...</h6>
+          }
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Meals;
