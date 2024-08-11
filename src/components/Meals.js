01d8264b95ffc089b115827ac916fca62d533cb4
meskatabusayed <meskatabusayed@gmail.com>
2024-08-11 14:32:32 +0530

    data fetching done

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -4,10 +4,11 @@ import React, { useEffect, useState }  from 'react'
 const Meals = () => {
   const [search , setSearch] = useState("a");
   const [meals , setMeals] = useState([]);
+  
 
   const loadData = async() => {
     const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`);
-    const data = res.json();
+    const data = await res.json();
     setMeals(data.meals);
   }
 
@@ -18,9 +19,9 @@ const Meals = () => {
 
   }
 
-  // useEffect(() => {
-  //    loadData();
-  // } , [search]);
+  useEffect(() => {
+     loadData();
+  } , [search]);
   
   return (
     <div>
@@ -28,6 +29,17 @@ const Meals = () => {
       onChange={handler}
       type="text" placeholder='search meals...'/>
       <button onClick={() => loadData()} className='bg-blue-700 p-2 text-white rounded-md font-bold'>Search</button>
+      <div className='mt-12 grid grid-cols-3 gap-6'>
+        {
+          meals?.map((meal) => (
+            <div key={meal.idMeal} className='border-2 p-4'>
+              <h6>{meal.strMeal}</h6>
+              <p>{meal.strInstructions}</p>
+            </div>
+          ))
+        }
+
+      </div>
     </div>
   )
 }
