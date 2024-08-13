b55df5ecad6ab132c030217050167bb39ca97248
meskatabusayed <meskatabusayed@gmail.com>
2024-08-13 05:39:42 +0530

    chaching and revalidate done

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 import { Headland_One } from 'next/font/google';
 
 const headland = Headland_One({weight : ["400"] , subsets: ["latin"]})
-
+//{cache : "no-store"}
 const getTime = async () => {
-    const res = await fetch("http://localhost:3000/time");
+    const res = await fetch("http://localhost:3000/time" , {next : {revalidate : 5 }});
     const data = await res.json();
     return data.currentTime;
 }
