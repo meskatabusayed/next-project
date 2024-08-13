33abe07714de8d7ab07bf8c57493ad2a7c9c50af
meskatabusayed <meskatabusayed@gmail.com>
2024-08-13 05:33:10 +0530

    dynamic time added

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -4,10 +4,18 @@ import { Headland_One } from 'next/font/google';
 
 const headland = Headland_One({weight : ["400"] , subsets: ["latin"]})
 
-const page = () => {
+const getTime = async () => {
+    const res = await fetch("http://localhost:3000/time");
+    const data = await res.json();
+    return data.currentTime;
+}
+
+const page = async () => {
+    const currentTime = await getTime(); 
   return (
     <div className={`${headland.className}`}>
       This is Gallery Page
+      <h6 className='text-red-600'>{currentTime}</h6>
     </div>
   )
 }
