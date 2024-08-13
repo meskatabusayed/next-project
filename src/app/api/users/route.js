befdf81f0a377f7b9cabaa309e20d21b6218cb7d
meskatabusayed <meskatabusayed@gmail.com>
2024-08-13 11:38:39 +0530

    get request done

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,6 +1,22 @@
 export async function GET(){
 
     return Response.json({
-        grettings : "Hello Sir"
+        comments
     })
 }
+
+
+const comments = [
+    {
+        id : 1,
+        message : "comment 1"
+    },
+    {
+        id : 2,
+        message : "comment 2"
+    },
+    {
+        id : 3,
+        message : "comment 3"
+    },
+]
