e4c4f887efe49ba52ed4d2e36ecb4175fe1e2193
meskatabusayed <meskatabusayed@gmail.com>
2024-08-13 10:49:55 +0530

    env file create done

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -3,12 +3,12 @@ import { redirect } from 'next/navigation';
 import React from 'react'
 
 const getPosts = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
     const data = await res.json();
-    if(data){
-      redirect(`/posts/${data[0].id}`)
-    }
-    return data
+    // if(data){
+    //   redirect(`/posts/${data[0].id}`)
+    // }
+    return data;
     
 
 }
