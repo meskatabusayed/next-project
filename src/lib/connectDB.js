1c7b207c9e14a309b025b830498527bee20005f5
meskatabusayed <meskatabusayed@gmail.com>
2024-08-19 08:46:20 +0530

    database file create

---
+++ src/lib/connectDB.js
@@ -0,0 +1,27 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+
+let db;
+const connectDB = async () => {
+    if(db) return db;
+    try {
+        const uri = process.env.NEXT_PUBLIC_MONGODB_URI
+        const client = new MongoClient(uri , {
+            serverApi : {
+                version : ServerApiVersion.v1,
+                strict : true,
+                deprecationErrors : true,
+            }
+
+        })
+
+        db = client.db("next-project");
+        return db;
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export default connectDB;
